31f6cb880817dd568557cf63f590861bac8615bf
lateralusX <lateralusx.github@gmail.com>
2025-08-14 12:56:04 +0200

    Debugger assert fixes.

--- src/coreclr/debug/ee/controller.cpp
+++ src/coreclr/debug/ee/controller.cpp
@@ -3894,9 +3894,9 @@ void DebuggerController::DispatchMethodEnter(void * pIP, FramePointer fp)
         {
             if ((p->GetThread() == NULL) || (p->GetThread() == pThread))
             {
-                ++count;
                 p->TriggerMethodEnter(pThread, dji, (const BYTE *) pIP, fp);
             }
+            ++count;
         }
         p = p->m_next;
     }
